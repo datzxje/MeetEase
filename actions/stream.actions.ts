971f73370a940aad305eabab99eac2d1264b52ab
datzxje <tudabest12@gmail.com>
2024-11-26 17:14:23 +0700

    initial

---
+++ actions/stream.actions.ts
@@ -0,0 +1,4 @@
+"use server"
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
---
+++ app/(root)/(home)/home/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import MeetingTypeList from '@/components/MeetingTypeList';
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const formattedDate = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(time);
+
+  return (
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
+            Upcoming Meeting at 12:30 PM
+          </h2>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-4xl font-extrabold lg:text-7xl'>
+              {formattedTime}
+            </h1>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
+              {formattedDate}
+            </p>
+          </div>
+        </div>
+      </div>
+      
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
