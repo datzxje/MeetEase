ccd3fab59c8fa9640dbaf6dac459890d0430ba60
datzxje <tudabest12@gmail.com>
2025-01-17 11:51:05 +0700

    update

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -5,28 +5,39 @@ import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
 
 import Loader from '@/components/Loader';
 import { useAuthContext } from '@/context/AuthContext';
+import { tokenProvider } from '@/actions/stream.actions';
 
 const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
-  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-  const {user, accessToken} = useAuthContext();
+  const [videoClient, setVideoClient] = useState<StreamVideoClient | null>(null);
+  const { user } = useAuthContext();
 
   useEffect(() => {
-    if (!user || !accessToken) return;
-    if (!API_KEY) throw new Error('Stream API key is missing');
+    const initializeClient = async () => {
+      if (!user) return;
+      if (!API_KEY) {
+        console.error('Stream API key is missing');
+        return;
+      }
 
-    const client = new StreamVideoClient({
-      apiKey: API_KEY,
-      user: {
-        id: user?.id.toString(),
-        name: user?.name || user?.id.toString(),
-      },
-      token: accessToken
-    });
-    console.log('User:', user);
-    console.log('Access Token:', accessToken);
-    setVideoClient(client);
+      try {
+        const client = new StreamVideoClient({
+          apiKey: API_KEY,
+          user: {
+            id: user.id.toString(),
+            name: user.name,
+          },
+          tokenProvider: () => tokenProvider(user.id.toString()),
+        });
+
+        setVideoClient(client);
+      } catch (error) {
+        console.error('Error initializing StreamVideoClient:', error);
+      }
+    };
+
+    initializeClient();
   }, [user]);
 
   if (!videoClient) return <Loader />;
